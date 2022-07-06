9c95af074b11bc55087005f771b8a0cc37b2ff47
Guy Marshall <guymarshall9@gmail.com>
2022-07-06 20:12:56 +0100

    Initial commit

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Factorial {
+import java.math.BigInteger;
+
+public class Factorial {
+    public BigInteger calculateFactorial(int number) {
+        BigInteger result = BigInteger.ONE;
+        BigInteger counter = BigInteger.TWO;
+        BigInteger userInput = new BigInteger(Integer.toString(number));
+
+        if (number > 1) {
+            while (counter.compareTo(userInput) < 1) { // counter <= number
+                result = result.multiply(counter);
+                counter = counter.add(BigInteger.ONE);
+            }
+        }
+        return result;
+    }
 }
--- src/Prime.java
+++ src/Prime.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Prime {
+import static java.lang.Math.ceil;
+import static java.lang.Math.sqrt;
+
+
+public class Prime {
+    public boolean isPrimeNumber(int number) {
+        int ceilingRoot = (int)ceil(sqrt(number));
+        if (ceilingRoot >= number) {
+            ceilingRoot = number - 1;
+        }
+
+        if (number % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i <= ceilingRoot; i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
