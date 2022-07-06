331db368167ed8eba6494a5bcf777e366758b840
Guy Marshall <guymarshall9@gmail.com>
2022-07-06 21:25:22 +0100

    Implement functions - isPrimeNumber() not working correctly

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    public static int saveInput(String prompt) {
+        System.out.println(prompt);
+        return scanner.nextInt();
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number = saveInput("Enter number to calculate factorial of:");
+
+        Factorial oFactorial = new Factorial();
+        BigInteger factorial = oFactorial.calculateFactorial(number);
+
+        Prime oPrime = new Prime();
+        while (true) {
+            boolean isPrime = oPrime.isPrimeNumber(factorial);
+            if (isPrime) {
+                break;
+            }
+            factorial = factorial.add(BigInteger.ONE);
+        }
+
+//        System.out.printf("%.3E%n", factorial);
+        System.out.println(factorial);
     }
 }
--- src/Prime.java
+++ src/Prime.java
@@ -1,20 +1,36 @@
+import java.math.BigInteger;
+
 import static java.lang.Math.ceil;
 import static java.lang.Math.sqrt;
 
 
 public class Prime {
-    public boolean isPrimeNumber(int number) {
-        int ceilingRoot = (int)ceil(sqrt(number));
-        if (ceilingRoot >= number) {
-            ceilingRoot = number - 1;
+    public boolean isPrimeNumber(BigInteger number) {
+        BigInteger ceilingRoot = number.sqrt();
+//        int ceilingRoot = (int)ceil(sqrt(number));
+//        if (ceilingRoot >= number) {
+//            ceilingRoot = number - 1;
+//        }
+        if (ceilingRoot.compareTo(number) >= 0) {
+            ceilingRoot = number.subtract(BigInteger.ONE);
         }
 
-        if (number % 2 == 0) {
+//        if (number % 2 == 0) {
+//            return false;
+//        }
+
+        if (number.mod(BigInteger.TWO).equals(BigInteger.ZERO)) {
             return false;
         }
 
-        for (int i = 3; i <= ceilingRoot; i += 2) {
-            if (number % i == 0) {
+//        for (int i = 3; i <= ceilingRoot; i += 2) {
+//            if (number % i == 0) {
+//                return false;
+//            }
+//        }
+
+        for (BigInteger i = BigInteger.valueOf(3); i.compareTo(ceilingRoot) >= 0; i = i.add(BigInteger.TWO)) {
+            if (number.mod(i).equals(BigInteger.ZERO)) {
                 return false;
             }
         }
